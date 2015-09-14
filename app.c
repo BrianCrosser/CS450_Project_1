4afcd92abadf548d79d2de8325b70fd7b0e2e1cf
Brian Crosser <crosserb@seawolf.sonoma.edu>
2015-09-14 13:45:50 -0700

    wc -l inputFile now works

--- app.c
+++ app.c
@@ -15,26 +15,31 @@ int main(int argc, char *argv[])
     
     int readFD;
     int pfd[2];
-
+    printf("%s\n", argv[1]);
+    /*
     if(pipe(pfd) == -1){
         fprintf(stderr, "Pipe Failed");
         exit(1);
     }
+    */
 
     pid_t childPID;
     switch(childPID = fork()){
         case -1: 
             fprintf(stderr, "Fork Failed");
         case 0:
-            if(readFD = open(argv[1], O_RDONLY) < 0)
+            if((readFD = open(argv[1], O_RDONLY)) < 0)
                 fprintf(stderr, "Open Failed");
-            if(dup2(readFD, 0) == -1 || dup2(pfd[1], 1) == -1 || close(pfd[0]) == -1 || close(pfd[1]) == -1 || close(readFD))
+            if(dup2(readFD, 0) == -1)
                 fprintf(stderr, "dup2 or clsoe Failed");
             execlp("wc", "wc", "-l", NULL);
     }
+    
+    /*
     if(close(pfd[0]) == -1 || close(pfd[1]) == -1)
         fprintf(stderr, "Parent pipes failed to close");
     while(wait(NULL) != -1)
         ;
+    */
     return 0;
 }
